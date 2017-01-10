dfc8a1e4bae6f7232821a48265dacbbffdd43f78
Aashish Nehete <ashnehete@gmail.com>
2017-01-10 18:46:15 +0530

    Initial commit

---
+++ logger.py
@@ -0,0 +1,35 @@
+import datetime
+
+# Getting the current day and updating the value
+dayf = open('day.int', 'r+')
+day = int(dayf.read())
+dayf.seek(0)
+dayf.write(str(day + 1))
+dayf.close()
+
+# Today's date in formatted string
+date = datetime.date.today().strftime("%d %B, %Y - %A")
+
+# Taking input
+progress = input('Today\'s Progress = ')
+thoughts = input('Thoughts = ')
+links = input('Links (comma separated) = ')
+
+# Creating log entry
+log = '### Day ' + str(day) + ': ' + date + '\n'
+log = log + '**Today\'s Progress:** ' + progress + '\n\n'
+log = log + '**Thoughts:** ' + thoughts + '\n\n'
+log = log + '**Link(s): **'
+
+# TODO: Check for no links
+links = links.split(',')
+for link in links:
+    link = link.split('-')
+    # TODO: Check for no title
+    log = log + '[' + link[0] + '](' + link[1] + ')\n'
+log = log + '\n\n'
+
+# Writing to log file
+f = open('log.md', 'a')
+f.write(log)
+f.close()
