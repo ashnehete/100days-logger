35e38964689fea9b3122329c4367f47f6f920cf7
Aashish Nehete <ashnehete@gmail.com>
2017-01-10 22:04:46 +0530

    minor bug fixes

--- logger.py
+++ logger.py
@@ -1,12 +1,5 @@
 import datetime
 
-# Getting the current day and updating the value
-dayf = open('day.int', 'r+')
-day = int(dayf.read())
-dayf.seek(0)
-dayf.write(str(day + 1))
-dayf.close()
-
 # Today's date in formatted string
 date = datetime.date.today().strftime("%d %B, %Y - %A")
 
@@ -15,11 +8,16 @@ progress = input('Today\'s Progress = ')
 thoughts = input('Thoughts = ')
 links = input('Links (comma separated) = ')
 
+# Getting the current day
+dayf = open('day.int', 'r')
+day = int(dayf.read())
+dayf.close()
+
 # Creating log entry
 log = '### Day ' + str(day) + ': ' + date + '\n'
 log = log + '**Today\'s Progress:** ' + progress + '\n\n'
 log = log + '**Thoughts:** ' + thoughts + '\n\n'
-log = log + '**Link(s): **'
+log = log + '**Link(s):** '
 
 # TODO: Check for no links
 links = links.split(',')
@@ -28,6 +26,11 @@ for link in links:
     # TODO: Check for no title
     log = log + '[' + link[0] + '](' + link[1] + ')\n'
 log = log + '\n\n'
+
+# Updating the day value
+dayf = open('day.int', 'w')
+dayf.write(str(day + 1))
+dayf.close()
 
 # Writing to log file
 f = open('log.md', 'a')
